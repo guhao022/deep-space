db216c9cd1d4161feda481f6a00a612dcd9ccbfa
guhao <378999587@qq.com>
2016-01-19 01:34:59 +0800

    fix

--- DLL/webman/error.go
+++ DLL/webman/error.go
@@ -1,5 +1,30 @@
 package webman
 
-const (
-	BAD
+import (
+	"github.com/num5/web"
+	"net/http"
 )
+
+const (
+	BadRequest = iota + 400
+	ResourceNotFound
+	PermissionDenied
+
+	ValidationError
+)
+
+func BadRequest() *web.Error {
+	return &web.Error{
+		ID: "400",
+		Links: &web.ErrLinks{
+			About: "http://golune.com",
+		},
+		Status: http.StatusBadRequest,
+		Code: "bad_request",
+		Title: "bad request",
+		Detail: "Bad Request",
+		Source: &web.ErrSource{
+
+		},
+	}
+}
