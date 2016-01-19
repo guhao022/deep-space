c6ac71983e54d4d58f5ab47be86d4040d35e1833
guhao <378999587@qq.com>
2016-01-20 00:19:53 +0800

    fix

--- DLL/webman/error.go
+++ DLL/webman/error.go
@@ -1,30 +1,53 @@
 package webman
-
 import (
-	"github.com/num5/web"
 	"net/http"
+	"encoding/json"
 )
 
-const (
-	BadRequest = iota + 400
-	ResourceNotFound
-	PermissionDenied
+type Error struct {
+	Id     string `json:"id"`
+	Status int    `json:"_"`
+	Title  string `json:"title"`
+	Detail string `json:"detail"`
+}
 
-	ValidationError
+func (err Error) Error() string {
+	return err.Detail
+}
+
+func NewError(w http.ResponseWriter, err Error) {
+	w.WriteHeader(err.Status)
+	w.Header().Set("content-type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(err)
+}
+
+
+var (
+	ErrBadRequest     = &Error{"bad_request", 400, "Bad request", "Request body is not well-formed. It must be JSON."}
+	ErrInternalServer = &Error{"internal_server_error", 500, "Internal Server Error", "Something went wrong."}
+	ErrNoData         = &Error{"no_data", 422, "No data", `Key "data" in the top level of the JSON document is missing or contains no data`}
+	ErrUnauthorized   = &Error{"unauthorized", 401, "Unauthorized", "Access token is invalid."}
+	ErrNotFound       = &Error{"not_found", 404, "Not found", "Route not found."}
 )
 
-func BadRequest() *web.Error {
-	return &web.Error{
-		ID: "400",
-		Links: &web.ErrLinks{
-			About: "http://golune.com",
-		},
-		Status: http.StatusBadRequest,
-		Code: "bad_request",
-		Title: "bad request",
-		Detail: "Bad Request",
-		Source: &web.ErrSource{
-
-		},
+func ErrMissParam(param string) *Error {
+	return &Error{
+		"miss_param",
+		400,
+		"Miss Param",
+		"miss query param: " + param,
 	}
+}
+
+type Success struct {
+	Status string      `json:"_"`
+	Title  string 	   `json:"title,omitempty"`
+	Data   interface{} `json:"data,omitempty"`
+}
+
+func Success(w http.ResponseWriter, title string, data interface{}) {
+	success := &Success{200, title, data}
+	w.WriteHeader(success.Status)
+	w.Header().Set("content-type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(success)
 }
--- DLL/webman/item.go
+++ DLL/webman/item.go
@@ -3,16 +3,58 @@ package webman
 import (
 	"github.com/num5/web"
 	"deep-space/DAL/model"
+	"net/http"
+	"strconv"
 )
 
-var item *model.Item
-
-func NewItem(ctx *web.Context) {
-	name := ctx.GetString("name")
+func NewItem(w http.ResponseWriter, r *http.Request) {
+	var err error
+	name := r.FormValue("name")
 	if name == "" {
-		ctx.Json("miss query param: name")
+		NewError(w, ErrMissParam("name"))
 		return
 	}
 
-	ctx.WriteString("aaaa")
+	cid := r.FormValue("cid")
+	if cid == "" {
+		NewError(w, ErrMissParam("cid"))
+		return
+	}
+
+	level := r.FormValue("level")
+	level, err = strconv.Atoi(level)
+	if err != nil || level == 0 {
+		NewError(w, ErrMissParam("level"))
+		return
+	}
+
+	price := r.FormValue("price")
+	price, err = strconv.Atoi(level)
+	if err != nil || price == 0 {
+		NewError(w, ErrMissParam("price"))
+		return
+	}
+
+	abstract := r.FormValue("abstract")
+	if abstract == "" {
+		NewError(w, ErrMissParam("abstarct"))
+		return
+	}
+
+	var item *model.Item
+
+	item.Name = name
+	item.Cid = cid
+	item.Level = level
+	item.Price = price
+	item.FallNum =  0
+	item.Abstract = abstract
+
+	err = item.AddItem()
+	if err != nil {
+		NewError(w, ErrInternalServer)
+		return
+	}
+
+	Success(w, "New Item", item.Id)
 }
